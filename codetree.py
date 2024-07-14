596ed6ceae18b8882c8eafead074eb505564952b
Lucas Hartmann <lucas.hartmann@gmail.com>
2024-07-13 21:23:13 -0300

    Rename parse_spec, and add generate_codes.

--- codetree.py
+++ codetree.py
@@ -8,7 +8,7 @@
 # take 0 means final code
 # use 0 means use as compression tokens.
 
-def parse_leaf(
+def parse_spec(
     spec, off, prefix,
     push_literal_code=lambda code:None,
     push_compression_range=lambda prefix,length,start:None
@@ -39,10 +39,21 @@ def parse_leaf(
         for u in range(use):
             suffix = "0" * take + f"{used+u:b}"
             suffix = suffix[-take:]
-            off2 = parse_leaf(spec, off, prefix+suffix, push_literal_code, push_compression_range)
+            off2 = parse_spec(spec, off, prefix+suffix, push_literal_code, push_compression_range)
         off = off2
         used += use
     return off
+
+def generate_symbols(spec):
+    literals = []
+    compress_ranges = []
+    def push_literal(literal):
+        literals.append(literal)
+    def push_compression_range(prefix, length, start):
+        compress_ranges.append((prefix, length, start))
+    parse_spec(spec, 0, "", push_literal, push_compression_range)
+    literals.sort(key = lambda literal: (len(literal),literal))
+    return literals, compress_ranges
 
 if __name__ == "__main__":
     print_code = lambda code: print(f"{code:<16} = Literals")
@@ -64,19 +75,18 @@ if __name__ == "__main__":
                     # all values were used
             # all values were used
     ]
-    parse_leaf(spec, 0, "", print_code, print_comp)
+    parse_spec(spec, 0, "", print_code, print_comp)
 
     spec_mini_1 = [
         0, # takes none: all literals
     ]
-    parse_leaf(spec_mini_1, 0, "LIT_ONLY", print_code, print_comp)
+    parse_spec(spec_mini_1, 0, "LIT_ONLY", print_code, print_comp)
 
     spec_mini_2 = [
         1, # takes 1 bit
         0 # use none for literals. Compression codes
     ]
-    parse_leaf(spec_mini_2, 0, "COMP_ONLY", print_code, print_comp)
+    parse_spec(spec_mini_2, 0, "COMP_ONLY", print_code, print_comp)
 
-
-
-
+    L, C = generate_symbols(spec)
+    print(L, C)
